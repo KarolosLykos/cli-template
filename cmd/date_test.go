1165a0be5d353f27ff3e640f809937ca458a7cd1
karolos lykos <l.g.karolos@gmail.com>
2022-09-06 22:35:26 +0300

    mod: change date command tests

--- cmd/date_test.go
+++ cmd/date_test.go
@@ -20,55 +20,55 @@ var testCases = []struct {
 	{
 		name:     "default",
 		args:     nil,
-		expected: time.Now().Format("02 Jan 06") + "\n",
+		expected: "02 Jan 06",
 		err:      false,
 	},
 	{
 		name:     "02 01 06",
 		args:     []string{"-f", "02 Jan 06"},
-		expected: time.Now().Format("02 Jan 06") + "\n",
+		expected: "02 Jan 06",
 		err:      false,
 	},
 	{
 		name:     "02 01 06 15:04:05",
 		args:     []string{"-f", "02 01 06 15:04:05"},
-		expected: time.Now().Format("02 01 06 15:04:05") + "\n",
+		expected: "02 01 06 15:04:05",
 		err:      false,
 	},
 	{
 		name:     "02 Jan 06",
 		args:     []string{"-f", "02 Jan 06"},
-		expected: time.Now().Format("02 Jan 06") + "\n",
+		expected: "02 Jan 06",
 		err:      false,
 	},
 	{
 		name:     "02 Jan 06 15:04:05",
 		args:     []string{"-f", "02 Jan 06 15:04:05"},
-		expected: time.Now().Format("02 Jan 06 15:04:05") + "\n",
+		expected: "02 Jan 06 15:04:05",
 		err:      false,
 	},
 	{
 		name:     "Mon Jan 06",
 		args:     []string{"-f", "Mon Jan 06"},
-		expected: time.Now().Format("Mon Jan 06") + "\n",
+		expected: "Mon Jan 06",
 		err:      false,
 	},
 	{
 		name:     "Mon Jan 06 15:04:05",
 		args:     []string{"-f", "Mon Jan 06 15:04:05"},
-		expected: time.Now().Format("Mon Jan 06 15:04:05") + "\n",
+		expected: "Mon Jan 06 15:04:05",
 		err:      false,
 	},
 	{
 		name:     "02 01 2006",
 		args:     []string{"-f", "02 01 2006"},
-		expected: time.Now().Format("02 01 2006") + "\n",
+		expected: "02 01 2006",
 		err:      false,
 	},
 	{
 		name:     "02 01 2006 15:04:05 MST",
 		args:     []string{"-f", "02 01 2006 15:04:05 MST"},
-		expected: time.Now().Format("02 01 2006 15:04:05 MST") + "\n",
+		expected: "02 01 2006 15:04:05 MST",
 		err:      false,
 	},
 	{
@@ -93,7 +93,7 @@ func TestDateCommand(t *testing.T) {
 		if tc.err {
 			assert.Error(t, err, tc.name)
 		} else {
-			assert.Equal(t, tc.expected, string(out), tc.name)
+			assert.Equal(t, time.Now().Format(tc.expected)+"\n", string(out), tc.name)
 		}
 	}
 }
